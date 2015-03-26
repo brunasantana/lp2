02709c421876b49b4bc32a767eac69327aba138d
brunasantana <brusantana@outlook.com.br>
2015-03-26 11:43:35 -0300

    Update ContaBancaria2.java
    corrigido

--- exercicio2/ContaBancaria2.java
+++ exercicio2/ContaBancaria2.java
@@ -2,7 +2,7 @@ public class ContaBancaria2{
 
         int agencia, contaCorrente;
         double saldo, limiteExtra;
-		String titularConta;
+	String titularConta;
 
         public void imprimirSaldo(){
                 System.out.println("Saldo: " + this.saldo);
@@ -21,38 +21,38 @@ public class ContaBancaria2{
                 System.out.println("A conta-corrente é: " + this.contaCorrente);
         }
 
-		public void imprimirTitular(){
-				System.out.println("O titular da conta é: " + this.titularConta);
-		}
+	public void imprimirTitular(){
+		System.out.println("O titular da conta é: " + this.titularConta);
+	}
 
         public static void main(String [] args){
 
-                ContaBancaria cb1= new ContaBancaria();			
-				ContaBancaria cb2= new ContaBancaria();
+                ContaBancaria2 cb1= new ContaBancaria2();			
+		ContaBancaria2 cb2= new ContaBancaria2();
 
                 cb1.agencia = 3610;
                 cb1.contaCorrente = 15766;
                 cb1.saldo = 35.54;
                 cb1.limiteExtra = 50.00;
-				cb1.titularConta = "Maria";   
+		cb1.titularConta = "Maria";   
 				
-				cb2.agencia = 810;
+		cb2.agencia = 810;
                 cb2.contaCorrente = 6132;
                 cb2.saldo = 42.31;
                 cb2.limiteExtra = 75.00;
-				cb2.titularConta = "João";
+		cb2.titularConta = "João";
 
-				cb1.imprimirTitular();
-				cb1.imprimirAgencia();
+		cb1.imprimirTitular();
+		cb1.imprimirAgencia();
                 cb1.imprimirContaCorrente();
-				cb1.imprimirSaldo();
+		cb1.imprimirSaldo();
                 cb1.imprimirSaldoTotal();
 
 
-				cb2.imprimirTitular();
-				cb2.imprimirAgencia();
+		cb2.imprimirTitular();
+		cb2.imprimirAgencia();
                 cb2.imprimirContaCorrente();
-				cb2.imprimirSaldo();
+		cb2.imprimirSaldo();
                 cb2.imprimirSaldoTotal();
         }
 
